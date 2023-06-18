836e662fc1ee58c30eab39f2364b372e9ad06549
Jimmy Chu <jim@rhetorai.com>
2023-06-18 22:18:26 +0800

    feat: Update meta for noscroll and Checkout script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" /> 
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <Script src="https://cdn.checkout.com/js/framesv2.min.js" strategy="beforeInteractive"></Script>
       </body>
     </Html>
   )
