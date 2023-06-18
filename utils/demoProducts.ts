8b67e00b9c8c50f71003f3b508ccf76caac57757
Jimmy Chu <jim@rhetorai.com>
2023-06-18 22:16:20 +0800

    feat: Add demo products data

---
+++ utils/demoProducts.ts
@@ -0,0 +1,13 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Cold Brew Bottle',
+    description:
+      'This glass bottle comes with a mesh insert for steeping tea or cold-brewing coffee. Pour from any angle and remove the top for easy cleaning.',
+    href: '#',
+    quantity: 1,
+    price: '$36.00',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/confirmation-page-05-product-01.jpg',
+    imageAlt: 'Glass bottle with black plastic pour top and mesh insert.',
+  },
+]
